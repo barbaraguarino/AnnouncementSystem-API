4f66cb844c91452f86ff148b018cdda65a3c7cc9
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-10-18 11:03:41 -0300

    Inicio

---
+++ src/main/java/com/system/announcement/AnnouncementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.system.announcement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnouncementSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnouncementSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/system/announcement/AnnouncementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.announcement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnouncementSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
