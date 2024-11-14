2ae1bb924281d7ba2d4b1e01dd9fa44090613c9c
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-11-14 18:42:46 -0300

    Mudando o tipo da lista de cidades e categorias no record do requestfilter

--- src/main/java/com/system/announcement/dtos/Announcement/requestFilterAnnouncementRecordDTO.java
+++ src/main/java/com/system/announcement/dtos/Announcement/requestFilterAnnouncementRecordDTO.java
@@ -4,12 +4,13 @@ import com.system.announcement.auxiliary.enums.AnnouncementStatus;
 import com.system.announcement.auxiliary.enums.UserType;
 
 import java.util.Set;
+import java.util.UUID;
 
 public record requestFilterAnnouncementRecordDTO(
         String title,
         String content,
-        Set<String> cities,
-        Set<String> categories,
+        Set<UUID> cities,
+        Set<UUID> categories,
         Float minPrice,
         Float maxPrice,
         UserType userType
