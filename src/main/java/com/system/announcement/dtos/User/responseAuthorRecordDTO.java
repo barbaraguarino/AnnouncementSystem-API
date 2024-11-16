34869059d161c6fdb0c4b6b0a2cda23611b82d77
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-11-16 15:26:21 -0300

    mudança no retorno do author do anuncio

--- src/main/java/com/system/announcement/dtos/User/responseAuthorRecordDTO.java
+++ src/main/java/com/system/announcement/dtos/User/responseAuthorRecordDTO.java
@@ -2,12 +2,15 @@ package com.system.announcement.dtos.User;
 
 import com.system.announcement.models.User;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record responseAuthorRecordDTO(
         @NotBlank String email,
-        @NotBlank String name
+        @NotBlank String name,
+        String icon,
+        @NotNull float score
 ) {
     public responseAuthorRecordDTO(User author) {
-        this(author.getEmail(), author.getName());
+        this(author.getEmail(), author.getName(), author.getIcon(), author.getScore());
     }
 }
