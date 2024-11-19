d1e28f52cbe90b96ecf234067bf154ba41ff30a4
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-11-19 19:39:43 -0300

    Repositórios

---
+++ src/main/java/com/system/announcement/repositories/ChatRepository.java
@@ -0,0 +1,17 @@
+package com.system.announcement.repositories;
+
+import com.system.announcement.models.Announcement;
+import com.system.announcement.models.Chat;
+import com.system.announcement.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ChatRepository extends JpaRepository<Chat, UUID> {
+    List<Chat> findByUser(User user);
+    List<Chat> findByAnnouncement(Announcement announcement);
+}
+
