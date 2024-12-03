92301fc8a3978e10c979e55ab48de5e9d0412b9f
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-12-03 16:57:45 -0300

    Criação da tabela

---
+++ src/main/java/com/system/announcement/auxiliary/id/FavoriteId.java
@@ -0,0 +1,21 @@
+package com.system.announcement.auxiliary.id;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FavoriteId implements Serializable {
+
+    private UUID announcement;
+    private String user;
+
+}
