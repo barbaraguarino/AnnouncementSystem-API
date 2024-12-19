8d8f6200146075a4d2eb5925833f3427b8f15e9a
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-12-19 14:39:45 -0300

    Mudança no enum do status do Chat

--- src/main/java/com/system/announcement/auxiliary/enums/ChatStatus.java
+++ src/main/java/com/system/announcement/auxiliary/enums/ChatStatus.java
@@ -2,17 +2,8 @@ package com.system.announcement.auxiliary.enums;
 
 public enum ChatStatus {
 
-    OPEN("Aberto"),
-    CLOSED("Fechado"),
-    DELETED("Deletado");
+    OPEN,
+    CLOSED,
+    DELETED
 
-    private final String status;
-
-    ChatStatus(String status) {
-        this.status = status;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
 }
