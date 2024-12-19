5bd65730c9f0041692fd4f95b4ea08b94e71d1eb
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-12-19 14:30:50 -0300

    Mudanças nas exceções

--- src/main/java/com/system/announcement/exceptions/NoAuthorizationException.java
+++ src/main/java/com/system/announcement/exceptions/NoAuthorizationException.java
@@ -4,6 +4,7 @@ public class NoAuthorizationException extends RuntimeException {
     public NoAuthorizationException() {
         super("No authorization required");
     }
+
     public NoAuthorizationException(String message) {
         super(message);
     }
