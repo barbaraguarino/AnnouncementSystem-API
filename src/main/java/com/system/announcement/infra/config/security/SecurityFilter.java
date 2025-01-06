88ee5ff68e27c4f7b710d486d67679a9f90ff880
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-01-06 17:36:07 -0300

    Concertando um warning

--- src/main/java/com/system/announcement/infra/config/security/SecurityFilter.java
+++ src/main/java/com/system/announcement/infra/config/security/SecurityFilter.java
@@ -2,6 +2,7 @@ package com.system.announcement.infra.config.security;
 
 import com.system.announcement.infra.token.TokenService;
 import com.system.announcement.services.AuthorizationService;
+import io.micrometer.common.lang.NonNull;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,37 +18,42 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
-    final TokenService tokenservice;
-
-    final AuthorizationService authorizationService;
+    private final TokenService tokenservice;
+    private final AuthorizationService authorizationService;
 
     public SecurityFilter(TokenService tokenservice, AuthorizationService authorizationService) {
         this.tokenservice = tokenservice;
         this.authorizationService = authorizationService;
     }
 
-
-
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
             throws ServletException, IOException {
+
         var token = this.recoverToken(request);
+
         if(token != null){
             var email = tokenservice.validateToken(token);
+
             if(!email.isEmpty()){
                 UserDetails user = authorizationService.loadUserByUsername(email);
-                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+                var authentication = new UsernamePasswordAuthenticationToken(user,
+                        null, user.getAuthorities());
+
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
+
         filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request){
         var authHeader = request.getHeader("Authorization");
         if(authHeader == null) return null;
+
         return authHeader.replace("Bearer ", "");
     }
 }
